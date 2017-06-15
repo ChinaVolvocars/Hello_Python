0f1381db404cafb44c26823331397e33361192c1
Tiger <atlantisspeed@gmail.com>
2017-06-15 13:27:56 +0800

    dict

---
+++ P05_dict_set.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+# dict和set
+# dict 全称dictionary ,其他语言称为map，使用键-值（key-value）进行存储，查找速度快
+name = ['D', 'C', 'B', 'A']
+fruit = ['74', '75', '76', '78']
+
+d = {'D': '4', 'C': '5', 'B': '6', 'A': '8'}
+print d['D']
