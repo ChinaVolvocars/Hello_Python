b0b56f20340cd803cc07f10c819c9a9473bc727f
Tiger <atlantisspeed@gmail.com>
2017-06-15 15:05:24 +0800

    dict

--- P05_dict_set.py
+++ P05_dict_set.py
@@ -3,7 +3,7 @@
 # dict和set
 # dict 全称dictionary ,其他语言称为map，使用键-值（key-value）进行存储，查找速度快
 name = ['D', 'C', 'B', 'A']
-fruit = ['74', '75', '76', '78']
+frui = ['4', '5', '6', '8']
 
 d = {'D': '4', 'C': '5', 'B': '6', 'A': '8'}
 print d['D']
