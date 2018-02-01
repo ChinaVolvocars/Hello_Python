a2049de66846bb5df98eb865adc392d2df8b5f38
Tiger <atlantisspeed@gmail.com>
2018-02-01 17:48:02 +0800

    高阶函数

--- lean/04-高级特征/04-生成器.py
+++ lean/04-高级特征/04-生成器.py
@@ -1,2 +1,86 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+# 一边循环一边计算的机制，称为生成器：generator。
+
+L = [x * x for x in range(10)]
+print(L)
+print('--------------------------------------------------')
+g = (x * x for x in range(10))
+print(g)
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+
+print('--------------------------------------------------')
+g1 = (x * x for x in range(10))
+for v in g1:
+    print(v)
+
+print('--------------------------------------------------')
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+
+print(fib(5))
+
+print('--------------------------------------------------')
+
+
+def fibs(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+
+generator = fibs(5)
+print(generator)
+
+print('--------------------------------------------------')
+
+
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield (3)
+    print('step 3')
+    yield (5)
+
+
+odd1 = odd()
+print(odd1)
+next(odd1)
+next(odd1)
+next(odd1)
+print('--------------------------------------------------')
+
+odd2 = odd()
+for nn in odd2:
+    print(nn)
+
+print('-----------------------sss---------------------------')
+
+while True:
+    try:
+        x = next(generator)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
--- lean/04-高级特征/05-迭代器.py
+++ lean/04-高级特征/05-迭代器.py
@@ -1,2 +1,42 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+from collections import Iterable
+
+# list 、tuple、dict、set、str
+# generator 包括生成器和带yield的generator function
+# 这些能直接for 循环的对象称为可迭代对象:Iterable
+isinstance([], Iterable)
+print(isinstance([], Iterable))
+print(isinstance({}, Iterable))
+print(isinstance('afvd', Iterable))
+print(isinstance((x for x in range(10)), Iterable))
+print(isinstance(100, Iterable))
+
+# 被next()函数调用并不断返回下一个值的对象称为迭代器：Iterator。
+from collections import Iterator
+
+print('---------------------------------')
+instance = isinstance((x for x in range(10)), Iterator)
+print(instance)
+
+print(isinstance([], Iterator))
+print(isinstance({}, Iterator))
+print(isinstance('abc', Iterator))
+
+print('---------------------------------')
+
+# 生成器都是Iterator对象，但list、dict、str虽然是Iterable，却不是Iterator。
+# 把list、dict、str等Iterable变成Iterator可以使用iter()函数
+print(isinstance(iter([]), Iterator))
+print(isinstance(iter({}), Iterator))
+print(isinstance(iter('abc'), Iterator))
+'''
+凡是可作用于for循环的对象都是Iterable类型；
+
+凡是可作用于next()函数的对象都是Iterator类型，它们表示一个惰性计算的序列；
+
+集合数据类型如list、dict、str等是Iterable但不是Iterator，不过可以通过iter()函数获得一个Iterator对象。
+
+Python的for循环本质上就是通过不断调用next()函数实现的，例如：
+
+'''
