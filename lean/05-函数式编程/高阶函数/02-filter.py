1786c26fd8bb000f5abe39a5a722b6bd16faec55
Tiger <atlantisspeed@gmail.com>
2018-02-02 17:31:37 +0800

    高阶函数

--- lean/05-函数式编程/高阶函数/02-filter.py
+++ lean/05-函数式编程/高阶函数/02-filter.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+# filter()用于过滤
+def is_odd(n):
+    return n % 2 == 1
+
+
+i = filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15])
+l = list(i)
+print(l)
+
+print('-----------------------------')
+
+
+def not_empty(s):
+    return s and s.strip()
+
+
+iterator = filter(not_empty, ['A', '', 'B', None, 'C', '  '])
+list1 = list(iterator)
+print(list1)
