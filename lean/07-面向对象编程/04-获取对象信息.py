435faf7a505c4cdc667a2e224b21d383884bd439
Tiger <atlantisspeed@gmail.com>
2018-02-06 15:23:41 +0800

    面向对象

--- lean/07-面向对象编程/04-获取对象信息.py
+++ lean/07-面向对象编程/04-获取对象信息.py
@@ -1,2 +1,48 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+# 使用type()
+
+print(type(123))
+print(type('222'))
+print(type(0.23))
+print(type(None))
+
+import types
+
+
+def nn():
+    pass
+
+
+function_type = type(nn) == types.FunctionType
+print(function_type)
+builtin_function_type = type(abs) == types.BuiltinFunctionType
+print(builtin_function_type)
+lambda_type = type(lambda x: x) == types.LambdaType
+print(lambda_type)
+generator_type = type((x for x in range(10))) == types.GeneratorType
+print(generator_type)
+
+# 使用isinstance()
+
+class Animal(object):
+    pass
+
+
+animal = Animal()
+
+
+class Dog(Animal):
+    pass
+
+
+dog = Dog()
+
+
+class Cat(Animal):
+    pass
+
+
+cat = Cat()
+
+print(isinstance(dog, Dog) and isinstance(dog, Animal))
